f88c150fe5bb9ee5469718bcc569bd49b3ac57d1
Davide <davide.deacetis13@gmail.com>
2022-08-21 15:14:07 +0200

    Fix css vista home

--- src/app/pages/recipes/recipes.page.ts
+++ src/app/pages/recipes/recipes.page.ts
@@ -70,6 +70,7 @@ export class RecipesPage implements OnInit {
     this.recipeService.getRandomRecipes().subscribe(
       (res) => {
         loading.dismiss();
+        console.log(...res.hits);
         this.recipes.push(...res.hits);
 
         event?.target.complete();
